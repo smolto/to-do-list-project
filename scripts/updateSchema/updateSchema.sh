5512ebc1dd681b33552a5c9148fbb8e0019d849d
Santiago Molto Martinez <santiagomoltomartinez@gmail.com>
2021-08-31 18:31:27 +0200

    Graphql folder created with schema file inside

--- scripts/updateSchema/updateSchema.sh
+++ scripts/updateSchema/updateSchema.sh
@@ -5,7 +5,7 @@ if test -f "$FILE"; then
   API_URL=`cat .env | grep API_URL | cut -d' ' -f2`
   API_KEY=`cat .env | grep API_KEY | cut -d' ' -f2`
 
-  jq -n --rawfile schema schema.graphql '{ query: "mutation updateGQLSchema($sch: String!) { updateGQLSchema(input: { set: { schema: $sch }}) { gqlSchema { schema } } }", variables: { sch: $schema }}' | 
+  jq -n --rawfile schema ../../graphql/schema.graphql '{ query: "mutation updateGQLSchema($sch: String!) { updateGQLSchema(input: { set: { schema: $sch }}) { gqlSchema { schema } } }", variables: { sch: $schema }}' | 
     curl "https://${API_URL}/admin" \
       -H "Content-Type: application/json" \
       -H "X-Auth-Token: ${API_KEY}" \
